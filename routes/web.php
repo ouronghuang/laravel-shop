929f93167dfb7067410bbfcde48e489fc188ec2c
欧荣凰 <512574561@qq.com>
2018-06-14 22:08:35 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('/test', function() {
             return 'Your email is verified';
         });
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     });
 });
