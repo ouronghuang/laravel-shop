a4aaf2f190a1af026f5bf1639c0ac70866d05f85
欧荣凰 <512574561@qq.com>
2018-06-14 21:11:03 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="javascript:void(0);" target="_blank">ORH</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="javascript:void(0);">联系我们</a></p>
+    </div>
+</footer>
